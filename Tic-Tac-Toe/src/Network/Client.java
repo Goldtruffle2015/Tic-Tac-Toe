90ed9c5845f9abc74970cf26c8a7acb3f2176a28
John Yu <goldtruffle2015@hotmail.com>
2020-05-11 15:52:21 -0600

    Set up the Client

--- Tic-Tac-Toe/src/Network/Client.java
+++ Tic-Tac-Toe/src/Network/Client.java
@@ -1,5 +1,35 @@
 package Network;
 
-public class Client {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Client {
+	// -- Attributes -- //
+	private String ipv4;
+	private int port;
+	private Socket socket;
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	
+	// -- Constructor -- //
+	public Client(String ipv4) throws UnknownHostException, IOException {
+		this.ipv4 = ipv4;
+		this.port = 10;
+		this.socket = new Socket(InetAddress.getByName(this.ipv4), this.port);
+		this.output = new ObjectOutputStream(this.socket.getOutputStream());
+		this.input = new ObjectInputStream(this.socket.getInputStream());
+	}
+	
+	// -- Methods -- //
+	public int[] getFromServer() throws ClassNotFoundException, IOException {
+		return (int[]) this.input.readObject();
+	}
+	
+	public void sendToServer(int[] pos) throws IOException {
+		this.output.writeObject(pos);
+	}
 }
